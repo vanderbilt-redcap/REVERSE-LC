2387c7468407b377835f6d95dab1db4f1ebd25bc
Carl Reed <carlreedw@gmail.com>
2022-03-01 12:40:50 -0600

    complete temp_report data output

--- temp_report.php
+++ temp_report.php
@@ -4,12 +4,12 @@
 $allSitesData = $module->getAllSitesData();
 $report = [
 	"sites_no_locality" => $module->all_sites_data->sites_no_locality,
-	// "allSitesData" => $allSitesData,
-	// "records" => $module->records,
-	// "edc_data" => $module->edc_data,
-	// "dags" => $module->dags,
-	// "regulatory_data" => $module->regulatory_data,
-	// "inclusion_data" => $module->getInclusionData()
+	"allSitesData" => $allSitesData,
+	"records" => $module->records,
+	"edc_data" => $module->edc_data,
+	"dags" => $module->dags,
+	"regulatory_data" => $module->regulatory_data,
+	"inclusion_data" => $module->getInclusionData()
 ];
 
 echo json_encode($report);
